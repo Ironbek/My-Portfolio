eb2106a9b935239e4f18ff47faf5a4f296c871e7
Bekzat <bekzatkg@gmail.com>
2018-11-03 23:23:07 +0600

    My first comit

---
+++ Lesson 1/js/main-two.js
@@ -0,0 +1,54 @@
+/*var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+var sym = Symbol();
+
+	//console.log(n1);  /*выведит null - так как значения n1 в коде нет */
+
+/*	var und;
+	console.log(und);
+
+ obj = {
+
+ 	'name': 'Bekzat',
+ 	'age': 21,
+ 	'isMarried': false
+ }
+
+ console.log(obj.name);*/
+
+/* confirm("Are you here ?");*/
+
+ /*var arr = ['Beka', 'Jekaa', 'Meka'];
+
+ 
+
+ //console.log(typeof(answer)); */
+
+
+/* var idChek = true;
+ var linkLine  = true;
+
+ console.log(idChek && !linkLine);*/
+
+
+// var answer = +prompt("Вам есть 18?", "18"); /*+ здесь преообразует строку в число*/
+
+
+
+	
+		var height = prompt("Веди высоту");
+
+		if (height < 23){
+
+			for(var i = 0; i < height; i++){
+
+		console.log();
+	}
+}
+	
+	
+
+
+
+	
